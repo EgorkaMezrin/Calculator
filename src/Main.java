f00255bd64f6e98c4442bb6528453a7b656406ff
EgorkaMezrin <95707316+EgorkaMezrin@users.noreply.github.com>
2023-01-06 00:04:48 +0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);
+        if (c == 0) {
+            System.out.println("zero devide");
+        } else {
+            calc.println.accept(c);
+        }
+        ;
+
+    }
+}
